14eff1c12378539957279839e14e7760cfdc2c3f
odeepeters <odeepeters@gmail.com>
2024-10-15 13:45:07 +0100

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>        
+        <a href="/">FunShop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
