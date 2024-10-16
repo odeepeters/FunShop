f4c6149b92ec0a78afaa4f5299089e8ec3a6203c
odeepeters <odeepeters@gmail.com>
2024-10-16 12:05:28 +0100

    Style products

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2. Install tools
 3. Create react app
 4. Create Repository
+5. List products
+    1. create products array
+    2. add product images
+    3. render products
+    4. style products 
+
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', //679px x 829px
+            price: 129,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 20,
+            description: 'High Quality Shirt'
+
+        },
+        {
+            name: 'Nike Sweat Pants',
+            slug: 'nike-sweat-pants',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 130,
+            countInStock: 100,
+            brand: 'Nike',
+            rating: 4.3,
+            numReviews: 20,
+            description: 'High Quality Pants'
+
+        },
+        {
+            name: 'Adidas Slim Shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 50,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.8,
+            numReviews: 50,
+            description: 'Good Quality Shirt'
+
+        },
+        {
+            name: 'Adidas Slim Pants',
+            slug: 'adidas-slim-pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 139,
+            countInStock: 200,
+            brand: 'Adidas',
+            rating: 4.9,
+            numReviews: 200,
+            description: 'High Quality Pants',
+
+        },
+    ],
+};
+
+export default data;
