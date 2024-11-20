51b36802449821bee950dbeddd8d611ba9044ff1
odeepeters <odeepeters@gmail.com>
2024-11-20 12:59:15 +0100

    Created a complete Sign-in Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { store } from './store';
 import CartScreen from './screens/CartScreen.js';
+import SigninScreen from './screens/SigninScreen.js';
 
 
 function App() {
@@ -41,6 +42,7 @@ function App() {
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen />} />
           <Route path="/cart" element={<CartScreen />} />
+          <Route path="/signin" element={<SigninScreen />} />
           <Route path="/" element={<HomeScreen />} />
         </Routes>
         </Container>
